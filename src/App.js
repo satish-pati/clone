e6e05d9d516dca07e447051b2d70ff9713e75d48
satish pati <satishpati2006@gmail.com>
2024-10-02 22:20:50 +0530

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,1 @@
+"import React from 'react';\nfunction App() {\n  return (\n    <div>\n      <h1>Welcome to Senior Browser</h1>\n    </div>\n  );\n}\nexport default App;" 
---
+++ src/index.js
@@ -0,0 +1,1 @@
+"import React from 'react';\nimport ReactDOM from 'react-dom';\nimport App from './App';\nReactDOM.render(<App />, document.getElementById('root'));" 
