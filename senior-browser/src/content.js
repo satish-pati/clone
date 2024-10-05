291d13ca61ca426127fe0b5f05bbb2775632b288
satish pati <satishpati2006@gmail.com>
2024-10-06 02:16:31 +0530

    added options.html

--- senior-browser/src/content.js
+++ senior-browser/src/content.js
@@ -170,7 +170,6 @@ const blurNegnews = () => {
 
     allNewstext.forEach((box) => {
         const newsText = box.innerText;
-
         if (containsNegtext(newsText)) {
             box.style.filter = "blur(8px)";
             box.style.transition = "filter 0.3s ease";
