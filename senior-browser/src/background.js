c01302dd6655c7a06ef55f435066e39cc77d9dd7
satish pati <satishpati2006@gmail.com>
2024-10-20 13:52:58 +0530

    Modified background.js

--- senior-browser/src/background.js
+++ senior-browser/src/background.js
@@ -17,3 +17,22 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       chrome.tts.speak(request.text, { rate: 1.0, pitch: 1.0, volume: 1.0 });
     }
   });
+  // background.js
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === 'translatePage') {
+      chrome.scripting.executeScript({
+          target: { tabId: sender.tab.id },
+          func: triggerGoogleTranslate
+      });
+  }
+});
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ language: 'en' }); // Default language is English
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'setLanguage') {
+    chrome.storage.sync.set({ language: request.language });
+    sendResponse({ status: 'success' });
+  }
+});
