e6d844ffdcabb538298e60de96a5ae7b9d76f125
satish pati <satishpati2006@gmail.com>
2024-11-04 20:13:02 +0530

    Modified content.js

--- senior-browser/src/content.js
+++ senior-browser/src/content.js
@@ -1,6 +1,5 @@
-//import { setupReadButton } from './modules/ScreenReader.js'
 //To change the button color and text on clicking
-injectButton();
+/*injectButton();
 //  detox search is only applied when the toggle is ON
 window.addEventListener('load', () => {
     if (isDetoxSearchOn) {
@@ -10,11 +9,11 @@ window.addEventListener('load', () => {
 });
 window.addEventListener('load', () => {
     document.body.appendChild(button);
-});
+});*/
 window.addEventListener('load', () => {
     createMainButton();
     createButtons(); 
-});
+});/*
 // Inject the button for font settings
 const button2 = document.createElement('button');
 button2.id = 'fontSettingsBtn';
@@ -34,4 +33,77 @@ document.body.appendChild(button2);
 // Load the font settings modal when the button is clicked
 button2.addEventListener('click', () => {
     loadFontSettingsModal();
+});*/
+window.addEventListener('load', () => {
+    injectFeatureButtons();
 });
+(() => {
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+    let currentVideoBookmarks = [];
+  
+    const fetchBookmarks = () => {
+      return new Promise((resolve) => {
+        chrome.storage.sync.get([currentVideo], (obj) => {
+          resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+        });
+      });
+    };
+  
+    const addNewBookmarkEventHandler = async () => {
+      const currentTime = youtubePlayer.currentTime;
+      const newBookmark = {
+        time: currentTime,
+        desc: "Bookmark at " + getTime(currentTime),
+      };
+  
+      currentVideoBookmarks = await fetchBookmarks();
+  
+      chrome.storage.sync.set({
+        [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+      });
+    };
+  
+    const newVideoLoaded = async () => {
+      const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+      currentVideoBookmarks = await fetchBookmarks();
+  
+      if (!bookmarkBtnExists) {
+        const bookmarkBtn = document.createElement("img");
+  
+        bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+        bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+        bookmarkBtn.title = "Click to bookmark current timestamp";
+  
+        youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+        youtubePlayer = document.getElementsByClassName('video-stream')[0];
+  
+        youtubeLeftControls.appendChild(bookmarkBtn);
+        bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+      }
+    };
+    chrome.runtime.onMessage.addListener((obj, sender, response) => {
+      const { type, value, videoId } = obj;
+      if (type === "NEW") {
+        currentVideo = videoId;
+        newVideoLoaded();
+      } else if (type === "PLAY") {
+        youtubePlayer.currentTime = value;
+      } else if ( type === "DELETE") {
+        currentVideoBookmarks = currentVideoBookmarks.filter((b) => b.time != value);
+        chrome.storage.sync.set({ [currentVideo]: JSON.stringify(currentVideoBookmarks) });
+  
+        response(currentVideoBookmarks);
+      }
+    });
+
+    newVideoLoaded();
+  })();
+  
+  const getTime = t => {
+    var date = new Date(0);
+    date.setSeconds(t);
+  
+    return date.toISOString().substr(11, 8);
+  };
+  
