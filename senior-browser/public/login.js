5c69b70a9e9eef36c21fd6be3a6624acc6be2203
satish pati <satishpati2006@gmail.com>
2024-11-06 23:54:33 +0530

    Added Login.js

---
+++ senior-browser/public/login.js
@@ -0,0 +1,30 @@
+const loginForm = document.getElementById('login-form');
+const message = document.getElementById('message');
+
+loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('http://localhost:3000/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password })
+        });
+
+        if (response.ok) {
+            chrome.storage.local.set({ isLoggedIn: true, username: username }, () => {
+                message.textContent = 'Login successful!';
+                // Redirect to the restricted page
+                window.location.href = "https://www.google.com/";
+            });
+            message.textContent = 'Login successful!';
+            
+        } else {
+            message.textContent = 'Invalid credentials';
+        }
+    } catch (error) {
+        message.textContent = 'Error logging in';
+    }
+});
