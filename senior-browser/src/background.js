a6d581c5c8b21fcabeb46730fb4a41cda18d702d
satish-pati <satishpati2006@gmail.com>
2024-11-11 18:38:35 +0530

    Update background.js

--- senior-browser/src/background.js
+++ senior-browser/src/background.js
@@ -1,49 +1,117 @@
 //import { MongoClient } from 'mongodb'; 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({ isLoggedIn: false });
-    console.log('insatlled');
+  chrome.storage.local.set({ isLoggedIn: false });
+  console.log('insatlled');
 });//just wrote for debugging 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url && tab.url.startsWith('http')) {
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ['src/content.js']
-        }).catch(error => {
-          console.log('error');
-        });
+chrome.runtime.onMessage.addListener((request, sender) => {
+if (request.url) {
+  chrome.downloads.download({
+    url: request.url,
+    saveAs: true
+  }, (downloadId) => {
+    if (chrome.runtime.lastError) {
+      console.error("Download failed:", chrome.runtime.lastError.message);
+    } else {
+      console.log("Download started with ID:", downloadId);
     }
+  });
+}
 });
-  
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'readContent') {
-      chrome.tts.speak(request.text, { rate: 1.0, pitch: 1.0, volume: 1.0 });
-    }
-  });
-  chrome.tabs.onUpdated.addListener((tabId, tab) => {
-    if (tab.url && tab.url.includes("youtube.com/watch")) {
-      const queryParameters = tab.url.split("?")[1];
-      const urlParameters = new URLSearchParams(queryParameters);
-  
-      chrome.tabs.sendMessage(tabId, {
-        type: "NEW",
-        videoId: urlParameters.get("v"),
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url && tab.url.startsWith('http')) {
+      chrome.scripting.executeScript({
+          target: { tabId: tabId },
+          files: ['src/content.js']
+      }).catch(error => {
+        console.log('error');
       });
-    }
-  });
+  }
+});
 
-  /*
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === "searchVideos") {
-        const sampleVideos = [
-            { title: "How to Use Chrome Extensions", url: chrome.runtime.getURL("videos/video2.mp4") }
-        ];
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === 'openreallocate') {
+    console.log("start");
+    chrome.windows.create({
+      url: chrome.runtime.getURL('src/reallocate.html'), 
+      type: 'popup',
+      width: 400,
+      height: 300
+    });
+  }
+if (request.action === 'opensecscan') {
+  console.log("start");
+  chrome.windows.create({
+    url: chrome.runtime.getURL('src/modules/sec.html'), 
+    type: 'popup',
+    width: 400,
+    height: 300
+    });
+  }
 
-        const filteredVideos = sampleVideos.filter(video =>
+  if (request.action === 'readContent') {
+    chrome.tts.speak(request.text, { rate: 1.0, pitch: 1.0, volume: 1.0 });
+  }
+  if (message.type === "searchVideos") {
+    db.collection('videos').find({}).toArray((err, videos) => {
+        if (err) {
+            console.error(err);
+            sendResponse({ videos: [] });
+            return;
+        }
+        const filteredVideos = videos.filter(video => 
             video.title.toLowerCase().includes(message.query.toLowerCase())
         );
-
         sendResponse({ videos: filteredVideos });
-    }
-    return true;
+    });
+}
+  
 });
+/*
+const { MongoClient } = require('mongodb');
+const url = 'mongodb+srv://Satish:Satya%4023@cluster0.8zasz.mongodb.net/videoLibrary?retryWrites=true&w=majority'
+const dbName = 'videoDB';
+let db;
+MongoClient.connect(url, { useUnifiedTopology: true })
+  .then(client => {
+      console.log("Connected to MongoDB");
+      db = client.db(dbName);
+  })
+  .catch(err => console.error(err));
+
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+      id: "reallocateFile",
+      title: "Reallocate an Rename File",
+      contexts: ["all"]
+  });
+});
+chrome.contextMenus.onClicked.addListener((info) => {
+  if (info.menuItemId === "renamefilesext") {
+    const fileId = info.targetElementId || null; 
+    chrome.windows.create({
+      url: chrome.runtime.getURL("/popup1/popup.html?fileId=" + fileId), 
+      type: "popup",
+      width: 600,
+      height: 300
+    });
+  }
+});
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "reallocateFile") {
+        chrome.tabs.create({ url: chrome.runtime.getURL("src/pop.html") });
+    }
+  });
+
 */
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete' && tab.url && tab.url.includes("youtube.com/watch")) {
+    const queryParameters = tab.url.split("?")[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+    });
+  }
+  });
+  
