40d7139267644ce55ec7eaf74876f04916e37ec4
satish-pati <satishpati2006@gmail.com>
2024-11-12 14:37:02 +0530

    Update login.js

--- senior-browser/public/login.js
+++ senior-browser/public/login.js
@@ -1,19 +1,21 @@
 const loginForm = document.getElementById('login-form');
 const message = document.getElementById('message');
-
+//add listner
 loginForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('https://render-nl4l.onrender.com/login', {
+        // fetch vidoes 
+        const response = await fetch('http://localhost:3000/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ username, password })
         });
 
         if (response.ok) {
+            //if successfull 
             chrome.storage.local.set({ isLoggedIn: true, username: username }, () => {
                 message.textContent = 'Login successful!';
                 // Redirect to the restricted page
